cc31f381b4bc13479620f3e76a37a412ba1254e7
Artem Dotsenko <artemdotsenkousa@gmail.com>
2021-01-22 13:49:38 +0700

    lesson 20

--- news/views.py
+++ news/views.py
@@ -5,19 +5,20 @@ from .models import News, Category
 
 def index(request):
     news = News.objects.order_by('created_at')
-    categories = Category.objects.all()
     context = {
         'news': news,
         'title': 'list of news',
-        'categories': categories,
     }
     return render(request, 'news/index.html', context)
 
 
 def get_category(request, category_id):
     news = News.objects.filter(category_id=category_id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'categories': categories, 'category': category})
+    context = {
+        'news': news,
+        'category': category,
+    }
+    return render(request, 'news/category.html', context)
 
 

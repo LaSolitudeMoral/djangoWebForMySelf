dc6e176e716b7b0c0aa79ffa4677c810a6a00a2b
artem dotsenko <artemdotsenkousa@gmail.com>
2021-01-24 00:26:14 +0700

    Lesson 30

--- news/forms.py
+++ news/forms.py
@@ -1,10 +1,35 @@
 from django import forms
-from .models import Category
+from .models import News, Category
+from django.core.exceptions import ValidationError
+import re
 
+
+# формы связанные с моделью
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+        }
+
+    # Validator for title textbox
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
 
 # формы не связанные с моделью
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+# class NewsForm(forms.Form):
+#    title = forms.CharField(max_length=150, label='Заголовок',
+#                            widget=forms.TextInput(attrs={'class': 'form-control'}))
+#    content = forms.CharField(label='Текст статьи', required=False,
+#                              widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+#    is_published = forms.BooleanField(label='Опубликована ли статья?', initial=True, required=False)
+#    category = forms.ModelChoiceField(queryset=Category.objects.all(), label='Категория', empty_label='Без категории',
+#                                     widget=forms.Select(attrs={'class': 'form-control'}))
+
--- news/views.py
+++ news/views.py
@@ -1,38 +1,99 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import News, Category
 from .forms import NewsForm
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse_lazy
 
 
-def index(request):
-    news = News.objects.order_by('created_at')
-    context = {
-        'news': news,
-        'title': 'list of news',
+class HomeNews(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+    # для статичных данных
+    extra_context = {
+        'title': 'Главная'
     }
-    return render(request, 'news/index.html', context)
+
+    # для динамических данных
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    context = {
-        'news': news,
-        'category': category,
-    }
-    return render(request, 'news/category.html', context)
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/index.html'
+    context_object_name = 'news'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'])
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {'news_item': news_item})
+class ViewNews(DetailView):
+    model = News
+    # pk_url_kwarg = 'news_id'
 
 
-def add_news(request):
-    if request.method == 'POST':
-        pass
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    # success_url = reverse_lazy('home')
 
+
+# def index(request):
+#     news = News.objects.order_by('created_at')
+#     context = {
+#         'news': news,
+#         'title': 'list of news',
+#     }
+#     return render(request, 'news/index.html', context)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     context = {
+#         'news': news,
+#         'category': category,
+#     }
+#     return render(request, 'news/category.html', context)
+
+
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {'news_item': news_item})
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
+
+# Форма не связанна с моделью
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             news = News.objects.create(**form.cleaned_data)
+#             # return redirect('add_news')
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})

7edc5ab36b5bf585e556c018e269739aabd9ef2f
artem dotsenko <artemdotsenkousa@gmail.com>
2021-01-25 22:09:20 +0700

    Lesson 41

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category')
+    list_display = ('id', 'title', 'created_at', 'updated_at', 'is_published', 'category', 'get_photo')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
+    # поля для вывода в окне редактора новости(например для отображения фотографии)
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="50">')
+        else:
+            return 'No photo'
+
+    get_photo.short_description = 'Photo image'
 
 
 class CategoryAdmin(admin.ModelAdmin):
@@ -19,3 +32,5 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(News, NewsAdmin)
 admin.site.register(Category)
+
+admin.site.site_title = 'Управление новостями'
